6602a51ae57252b19f2b1fccc0168d3d18dccadc
wanrong <wanrong@wondersgroup.com>
2020-01-10 16:56:48 +0800

    作者头像和blog详情修改

--- components/Author.js
+++ components/Author.js
@@ -4,9 +4,9 @@ import '../public/style/components/author.css'
 const Author = () => {
     return (
         <div className="author-div comm-box">
-            <div> <Avatar size={100} src="http://139.224.227.52/image/dxx.png"  /></div>
+            <div> <Avatar size={100} src="http://139.224.227.52/image/joker-logo.png"  /></div>
             <div className="author-introduction">
-                光头程序员，专注于WEB和移动前端开发。要录1000集免费前端视频的傻X。此地维权无门，此时无能为力，此心随波逐流。
+                She always tells me to smile, and put on a happy face.
                 <Divider>社交账号</Divider>
                 <Avatar size={28} icon="github" className="account"  />
                 <Avatar size={28} icon="qq"  className="account" />
--- pages/detailed.js
+++ pages/detailed.js
@@ -1,6 +1,9 @@
 import React,{useState} from 'react'
 import Head from 'next/head'
-import {Row, Col , Icon ,Breadcrumb  } from 'antd'
+import {Row, Col ,Affix, Icon ,Breadcrumb  } from 'antd'
+import ReactMarkdown from 'react-markdown'
+import MarkNav from 'markdown-navbar';
+import 'markdown-navbar/dist/navbar.css';
 
 import Header from '../components/Header'
 import Author from '../components/Author'
@@ -10,6 +13,40 @@ import '../public/style/pages/detailed.css'
 
 
 export default function detailed() {
+    let markdown='## P01:课程介绍和环境搭建\n' +
+    '[ **M** ] arkdown + E [ **ditor** ] = **Mditor**  \n' +
+    '> Mditor 是一个简洁、易于集成、方便扩展、期望舒服的编写 markdown 的编辑器，仅此而已... \n\n' +
+    '**这是加粗的文字**\n\n' +
+    '*这是倾斜的文字*`\n\n' +
+    '***这是斜体加粗的文字***\n\n' +
+    '~~这是加删除线的文字~~ \n\n'+
+    '\`console.log(111)\` \n\n'+
+    '## p02:来个Hello World 初始Vue3.0\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n'+
+    '***\n\n\n' +
+    '## p03:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '## p04:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '## p05:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '## p06:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '## p07:Vue3.0基础知识讲解\n' +
+    '> aaaaaaaaa\n' +
+    '>> bbbbbbbbb\n' +
+    '>>> cccccccccc\n\n'+
+    '``` var a=11; ```'
     return (
         <>
             <Head>
@@ -29,7 +66,7 @@ export default function detailed() {
 
                         <div>
                             <div className="detailed-title">
-                                React实战视频教程-技术胖Blog开发(更新08集)
+                                React实战
                             </div>
 
                             <div className="list-icon center">
@@ -39,7 +76,10 @@ export default function detailed() {
                             </div>
 
                             <div className="detailed-content" >
-                                详细内容，下节课编写
+                                <ReactMarkdown 
+                                    source={markdown} 
+                                    escapeHtml={false}  
+                                />
                             </div>
 
                         </div>
@@ -50,6 +90,17 @@ export default function detailed() {
                 <Col className="comm-right" xs={0} sm={0} md={5} lg={5} xl={5}>
                     <Author/>
                     <Advert/>
+                    <Affix offsetTop={0}>
+                        <div className="detailed-nav comm-box">
+                            <div className="nav-title">文章目录</div>
+                            <MarkNav
+                                className="article-menu"
+                                source={markdown}
+                                ordered={false}
+                                headingTopOffset={-50}
+                            />
+                        </div>
+                    </Affix>
                 </Col>
             </Row>
             <Footer/>
