8984929d0bd18ee20e310c90ce37715fd8fb8b5c
wanrong <wanrong@wondersgroup.com>
2020-04-03 16:58:05 +0800

    [blog]前台页面重置

--- components/Author.js
+++ components/Author.js
@@ -1,12 +1,14 @@
 import {Avatar,Divider} from 'antd'
 import '../public/style/components/author.css'
 
-const Author = () => {
+const Author =()=>{
+
     return (
         <div className="author-div comm-box">
             <div> <Avatar size={100} src="http://139.224.227.52/image/joker-logo.png"  /></div>
             <div className="author-introduction">
-                She always tells me to smile, and put on a happy face.
+                学无止境<br/>
+                Knowledge has no limit
                 <Divider>社交账号</Divider>
                 <Avatar size={28} icon="github" className="account"  />
                 <Avatar size={28} icon="qq"  className="account" />
@@ -14,7 +16,8 @@ const Author = () => {
 
             </div>
         </div>
-    );
+    )
+
 }
 
-export default Author;
+export default Author
---
+++ config/apiUrl.js
@@ -0,0 +1,8 @@
+let ipUrl = 'http://localhost:3100/' 
+
+let servicePath = {
+    getBlogList:ipUrl + 'blogList/getBlogList' ,  //  首页文章列表接口
+    getBlogDetailById:ipUrl + 'blogList/blogDetail?_id=',  // 文章详细页内容接口 ,需要接收参数
+    getCategoryList : ipUrl + 'category/getCategoryList'    // 博客分类列表
+}
+export default servicePath;
