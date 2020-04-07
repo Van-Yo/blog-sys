fb1fe5ce643ae0293b7655dd992f8518d3441b31
wanrong <wanrong@wondersgroup.com>
2020-04-07 15:10:55 +0800

    [blog-sys]新增分类查询博客列表模块

--- pages/list.js
+++ pages/list.js
@@ -1,18 +1,31 @@
-import React,{useState} from 'react'
+import React,{useState,useEffect} from 'react'
 import Head from 'next/head'
+import axios from 'axios'
 import {Row, Col , List ,Icon ,Breadcrumb  } from 'antd'
+import Link from 'next/link'
+import '../public/style/pages/index.css'
 import Header from '../components/Header'
 import Author from '../components/Author'
 import Advert from '../components/Advert'
 import Footer from '../components/Footer'
-const BlogList = () => {
-  const [ mylist , setMylist ] = useState(
-      []
-    )
+import servicePath  from '../config/apiUrl'
+const BlogList = ({query}) => {
+  console.log(query.id);
+  const [ mylist , setMylist ] = useState([])
+  useEffect(()=>{
+      const fetchData = async ()=>{
+        const result= await axios(servicePath.getBlogListByCategoryId+query.id).then(
+              (res)=>{
+                setMylist(res.data)
+              }
+            )
+      }
+      fetchData()
+  },[query.id])
   return (
       <>
         <Head>
-          <title>Home</title>
+          <title>疯狂程序猿-这是一个基于next.js的初级博客网站</title>
         </Head>
         <Header />
         <Row className="comm-main" type="flex" justify="center">
@@ -29,15 +42,19 @@ const BlogList = () => {
                       itemLayout="vertical"
                       dataSource={mylist}
                       renderItem={item => (
-                      <List.Item>
-                          <div className="list-title">{item.title}</div>
-                          <div className="list-icon">
-                          <span><Icon type="calendar" /> 2019-06-28</span>
-                          <span><Icon type="folder" /> 视频教程</span>
-                          <span><Icon type="fire" /> 5498人</span>
-                          </div>
-                          <div className="list-context">{item.context}</div>  
-                      </List.Item>
+                        <List.Item>
+                            <div className="list-title">
+                              <Link href={{pathname:'/detailed',query:{id:item._id}}}>
+                                <a>{item.title}</a>
+                              </Link>
+                            </div>
+                            <div className="list-icon">
+                            <span><Icon type="calendar" /> {item.date}</span>
+                            <span><Icon type="folder" /> {item.category}</span>
+                            <span><Icon type="fire" /></span>
+                            </div>
+                            <div className="list-context">{item.brief}</div>  
+                        </List.Item>
                       )}
                   />    
               </div>
@@ -45,14 +62,20 @@ const BlogList = () => {
     
           <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
             <Author />
-            <Advert />
+            {/* <Advert /> */}
           </Col>
         </Row>
         <Footer/>
      </>
     )
 }
-
-
+// BlogList.getInitialProps = async(context) => {
+//   let id =context.query.id
+//   const res = await axios.get(servicePath.getBlogListByCategoryId+id)
+//   return { data: res.data }
+// }
+BlogList.getInitialProps = ({query}) => {
+  return {query}
+}
 
 export default BlogList
