62324809f79c4432accdf0fc64e3fc9c855796fe
wanrong <wanrong@wondersgroup.com>
2020-04-09 15:54:11 +0800

    [blog-sys]改改样式

--- pages/detailed.js
+++ pages/detailed.js
@@ -83,7 +83,7 @@ const Detailed = (list) => {
         <Col className="comm-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
           {/* <Advert /> */}
-          <Affix offsetTop={5}>
+          <Affix offsetTop={8}>
             <div className="detailed-nav comm-box">
               <div className="nav-title">文章目录</div>
               {tocify && tocify.render()}
